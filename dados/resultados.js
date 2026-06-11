32abd1c800441ac20cc9a223c44598567041548e
Nelson Martins <nlsn.dba@gmail.com>
2026-06-11 17:46:52 -0300

    Forçando atualização das apostas

--- dados/resultados.js
+++ dados/resultados.js
@@ -6,6 +6,6 @@
 
 const RESULTADOS = {
     // Exemplo quando um jogo terminar:
-// 1: { casa: 1, fora: 0 },  // México 2 x 0 África do Sul
+    1: { casa: 2, fora: 0 },  // México 2 x 0 África do Sul
     // 2: { casa: 1, fora: 1 },  // Coreia do Sul 1 x 1 Tchéquia
 };
