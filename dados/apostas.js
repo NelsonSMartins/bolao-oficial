42ff0c59d0e2b10a18728de235354414bf631efd
Nelson Martins <nlsn.dba@gmail.com>
2026-06-11 13:36:01 -0300

    Atualiza dados do bolão

--- dados/apostas.js
+++ dados/apostas.js
@@ -6,6 +6,6 @@
 const APOSTAS = {
     // Exemplos:
     "1_Nelson": { casa: 2, fora: 0 },
-    // "1_Douglas": { casa: 1, fora: 0 },
+    "1_Douglas": { casa: 2, fora: 0 },
     // "2_Nelson": { casa: 1, fora: 1 },
 };
