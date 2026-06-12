11ffe8b96a370724acadf2d89ba1dc98bcc5ba3f
NelsonSMartins <nelson.silmar@gmail.com>
2026-06-12 18:39:18 -0300

    Atualizar o resultados.js

--- dados/resultados.js
+++ dados/resultados.js
@@ -8,5 +8,5 @@ const RESULTADOS = {
     // Exemplo quando um jogo terminar:
     1: { casa: 2, fora: 0 },  // México 2 x 0 África do Sul
     2: { casa: 2, fora: 1 },  // Coreia do Sul 1 x 1 Tchéquia
-    3: { casa: 0, fora: 1 }, // 'Canadá', timeFora: 'Bósnia e Herzegovina'
+    3: { casa: 1, fora: 1 }, // 'Canadá', timeFora: 'Bósnia e Herzegovina'
 };
