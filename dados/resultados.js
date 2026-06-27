11a9e7426ae6dc66813b6b5ca5fb5ed673b802fa
NelsonSMartins <nelson.silmar@gmail.com>
2026-06-27 19:15:49 -0300

    Atualizar o resultados.js

--- dados/resultados.js
+++ dados/resultados.js
@@ -104,8 +104,8 @@ const RESULTADOS = {
     // 27/06/2026 - Fase de Grupos
     65: { casa: 5, fora: 1 }, // Bélgica x Nova Zelândia
     66: { casa: 1, fora: 1 }, // Egito x Irã
-    // 67: { casa: 0, fora: 0 }, // Inglaterra x Panamá
-    // 68: { casa: 0, fora: 0 }, // Croácia x Gana
+    67: { casa: 0, fora: 0 }, // Inglaterra x Panamá
+    68: { casa: 1, fora: 0 }, // Croácia x Gana
     // 69: { casa: 0, fora: 0 }, // Portugal x Colômbia
     // 70: { casa: 0, fora: 0 }, // República Democrática do Congo x Uzbequistão
     // 71: { casa: 0, fora: 0 }, // Argentina x Jordânia
